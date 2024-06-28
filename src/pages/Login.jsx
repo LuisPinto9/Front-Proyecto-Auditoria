c74b07762f35341683a9e8dabc22eab719b95a7b
Luis Enrique Jr. Pinto Fuentes <91229941+LuisPinto9@users.noreply.github.com>
2024-06-28 15:20:25 -0500

    Arreglo de interfaz de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 
 const Login = () => {
   const [file, setFile] = useState(null);
+  const [response, setResponse] = useState("");
+
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
   };
@@ -16,12 +18,74 @@ const Login = () => {
     });
 
     const result = await response.text();
-    console.log(result);
+    setResponse(result);
   };
+
   return (
-    <div className="App">
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={uploadFile}>Upload</button>
+    <div className="bg-gradient-primary" style={{ height: "100vh" }}>
+      <div className="container h-100">
+        <div className="row justify-content-center align-items-center h-100">
+          <div className="col-md-9 col-lg-12 col-xl-10">
+            <div className="card shadow-lg o-hidden border-0 my-5">
+              <div className="card-body p-0">
+                <div className="row">
+                  <div className="col-lg-6 d-none d-lg-flex">
+                    <div
+                      className="flex-grow-1 bg-login-image"
+                      style={{
+                        backgroundImage: `url(${
+                          response ? response : "images/dogs/image3.jpeg"
+                        })`,
+                      }}
+                    ></div>
+                  </div>
+                  <div className="col-lg-6">
+                    <div className="p-5">
+                      <div className="text-center">
+                        <h4 className="text-dark mb-4">Inicio de Sesión</h4>
+                      </div>
+                      <form className="user" id="login-form">
+                        <div className="mb-3">
+                          <input
+                            className="form-control form-control-user"
+                            type="email"
+                            id="exampleInputEmail"
+                            aria-describedby="emailHelp"
+                            placeholder="Enter Email Address..."
+                            name="email"
+                          />
+                        </div>
+                        <div className="mb-3">
+                          <input
+                            className="form-control form-control-user"
+                            type="password"
+                            id="exampleInputPassword"
+                            placeholder="Password"
+                            name="password"
+                          />
+                        </div>
+                        <div className="mb-3">
+                          <div className="custom-control custom-checkbox small"></div>
+                        </div>
+                        <button
+                          className="btn btn-primary d-block btn-user w-100"
+                          type="submit"
+                          id="login-form"
+                        >
+                          Iniciar Sesión
+                        </button>
+                      </form>
+                      <input type="file" onChange={handleFileChange} />
+                      <button onClick={uploadFile}>Upload</button>
+                      <img src={response} alt="" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -11,11 +11,10 @@ const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-      <Route exact path="/" Component={ListStudents} />
-      <Route exact path="/login" Component={Login} />
+      <Route exact path="/" Component={Login} />
         {/* <Route exact path="/" Component={Dashboard} /> */}
         <Route exact path="/listTopic" Component={ListTopic} />
-        {/* <Route exact path="/listStudents" Component={ListStudents} /> */}
+        <Route exact path="/listStudents" Component={ListStudents} />
         <Route exact path="/listInscription" Component={ListInscription} />
         <Route exact path="/listGroups" Component={ListGroups} />
       </Routes>
