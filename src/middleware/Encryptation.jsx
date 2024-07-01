df35583b2bed5f321e80efa14506f10417444be8
Luis Enrique Jr. Pinto Fuentes <91229941+LuisPinto9@users.noreply.github.com>
2024-07-01 10:34:09 -0500

    Arreglo de encryptation

---
+++ src/middleware/Encryptation.jsx
@@ -0,0 +1,46 @@
+import CryptoJS from "crypto-js";
+
+const encrypt = (text) => {
+  const key = CryptoJS.enc.Utf8.parse(import.meta.env.VITE_SECRET_KEY);
+  const iv = CryptoJS.enc.Utf8.parse(import.meta.env.VITE_SECRET_IV);
+
+  const encrypted = CryptoJS.AES.encrypt(text, key, {
+    iv: iv,
+    mode: CryptoJS.mode.CBC,
+    padding: CryptoJS.pad.Pkcs7,
+  });
+
+  const ivHex = CryptoJS.enc.Hex.stringify(iv);
+  const encryptedHex = encrypted.ciphertext.toString(CryptoJS.enc.Hex);
+
+  return ivHex + ":" + encryptedHex;
+};
+
+const decrypt = (text) => {
+  const textParts = text.split(":");
+  const ivHex = textParts.shift();
+  const encryptedText = textParts.join(":");
+
+  const iv = CryptoJS.enc.Hex.parse(ivHex);
+  const key = CryptoJS.enc.Utf8.parse(import.meta.env.VITE_SECRET_KEY);
+
+  try {
+    const decrypted = CryptoJS.AES.decrypt(
+      {
+        ciphertext: CryptoJS.enc.Hex.parse(encryptedText),
+      },
+      key,
+      {
+        iv: iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7,
+      }
+    ).toString(CryptoJS.enc.Utf8);
+    return decrypted;
+  } catch (error) {
+    console.error("Error al descifrar:", error);
+    return "";
+  }
+};
+
+export { encrypt, decrypt };
